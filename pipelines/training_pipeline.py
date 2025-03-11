c48a01f9d5398bd73a97049191cfe498bbfb7f87
michaWorku <mikias27worku@gmail.com>
2025-03-11 12:08:52 +0300

    feat: Implement data splitting step with Strategy Pattern
    Description:
    - Setup logging configuration for degugging and tracking
    - Added the DataSplittingStrategy abstract base class and concrete implementation (SimpleTrainTestSplitStrategy).
    - Implemented the DataSplitter context class to split the dataset using the selected strategy.
    - Added functionality to:
        - Split the dataset into training and testing sets using a simple train-test split.
        - Switch between different data splitting strategies dynamically.
    - Integrated the data splitting step into the ML pipeline.
    - Included unit tests for data splitting.

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -2,6 +2,7 @@ from steps.data_ingestion_step import data_ingestion_step
 from steps.handle_missing_values_step import handle_missing_values_step
 from steps.feature_engineering_step import feature_engineering_step
 from steps.outlier_detection_step import outlier_detection_step
+from steps.data_splitter_step import data_splitter_step
 from zenml import Model, pipeline, step
 
 
@@ -33,6 +34,7 @@ def ml_pipeline():
 
 
     # Data Splitting Step
+    X_train, X_test, y_train, y_test = data_splitter_step(clean_data, target_column="SalePrice")
 
 
     # Model Building Step
